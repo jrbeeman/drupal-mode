ae6833efe16ade6a037b20fde28ffd37ddc879b9
Jeff Beeman <jeff@jeffbeeman.com>
2012-02-20 09:18:30 -0700

    Initial import of drupal-mode

---
+++ README.md
@@ -0,0 +1,9 @@
+# Description
+
+A simple Drupal mode for Emacs based on PHP mode.
+
+# See also
+
+* [php-mode](https://github.com/ejmr/php-mode)
+* [Drupal.org discussion about Emacs configuration](http://drupal.org/node/59868)
+* [Emacs IDE for Drupal project](http://drupal.org/project/emacs)
---
+++ drupal-mode.el
@@ -0,0 +1,19 @@
+;;; drupal-mode.el --- major mode for Drupal coding
+;;; Requires php-mode: https://github.com/ejmr/php-mode
+
+;;;###autoload
+(define-derived-mode drupal-mode php-mode "Drupal"
+  "Major mode for Drupal coding.\n\n\\{drupal-mode-map}"
+  (setq tab-width 2)
+  (setq c-basic-offset 2)
+  (setq indent-tabs-mode nil)
+  (setq fill-column 78)
+  (setq show-trailing-whitespace t)
+  (add-hook 'before-save-hook 'delete-trailing-whitespace)
+  (c-set-offset 'case-label '+)
+  (c-set-offset 'arglist-close 0)
+  (c-set-offset 'arglist-intro '+) ; for FAPI arrays and DBTNG
+  (c-set-offset 'arglist-cont-nonempty 'c-lineup-math) ; for DBTNG fields and values
+  (run-mode-hooks 'drupal-mode-hook)
+)
+(provide 'drupal-mode)
